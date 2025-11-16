bcf67cde9315e0020fc61be8302e6bf9b59d1b33
naideyy0-ship-it <naideyy0@gmail.com>
2025-11-16 16:37:14 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "MontajMarket Backend Çalışıyor 🚀"}
+
+@app.get("/test")
+def test_endpoint():
+    return {"status": "OK", "info": "API başarılı şekilde yayında!"}
